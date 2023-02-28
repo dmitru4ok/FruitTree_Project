a98d5c646be88919c2b30ccfaa9ba75456ff8542
dmitru4ok <mobilokh4566@gmail.com>
2023-02-28 10:55:38 +0200

    Added project structure, initial commit

---
+++ FruitTree_Project/FtuitTree.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+namespace FruitTree_Project
+{
+    class FtuitTree
+    {
+    }
+}
---
+++ FruitTree_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace FruitTree_Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine();
+        }
+    }
+}
